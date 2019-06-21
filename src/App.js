5d88cf0447d5f8f8baa8011cc439cdbcaf97007c
1Amdro <halgadam@gmail.com>
2019-06-21 23:59:07 +0300

    2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const numb = 2;
+const flag = true;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p style={{
+          color:'purple',
+          fontSize:20
+        }}>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {numb},
+          {flag&& ' TRUE '},
+          {numb +2},
+          {flag ? 'YES' : 'NO'}
+          {undefined}
+          {null}
+          {false}
+          {true}
         </p>
         <a
           className="App-link"
