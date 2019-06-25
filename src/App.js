73c2fea27bb553f011cb1e2873a79b45abd1556e
1Amdro <halgadam@gmail.com>
2019-06-25 18:16:07 +0300

    React--component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const element = (<h1>
-  Hello New World!
-</h1>)
+const ItemList = () => (<ul>
+  <li>111</li>
+  <li>222</li>
+  <li>333</li>
+</ul>);
 
-export {element};
+const App = () => (<div>
+  <h1>Hello New World!</h1>
+    <ItemList/>
+</div>);
+
+export default App;
