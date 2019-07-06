6e50acca9354a474bfc72a2e1df40de7c223c2d3
1Amdro <halgadam@gmail.com>
2019-07-06 22:24:38 +0300

    style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,38 @@
 import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
-import InputItem from '../InputItem/InputItem';
 import styles from './App.module.css';
+import InputItem from '../InputItem/InputItem';
 
 const App = () => {
-  const items = [
-    {
-      value: 'Повысить мотивацию',
-      isDone: true
-    },
-    {
-      value: 'Сделать задачу',
-      isDone: false
-    },
-    {
-      value: 'Отдохнуть',
-      isDone: true
-    }
-  ];
 
-  return(
-    <div className={styles.wrap}>
-      <h1 className={styles.title}>Важные Дела:</h1>
-      <InputItem />
-      <ItemList items={items}/>
-      <Footer count={3} />
-    </div>);
-  }
+    const tasks = [
+        {
+            text: 'Повысить мотивацию',
+            isDone: true
+        },
+        {
+            text: 'Сделать задачу',
+            isDone: false
+        },
+        {
+            text: 'Отдохнуть',
+            isDone: true
+        },
+        {
+            text: 'Играть',
+            isDone: false
+        }
+    ]
 
+    let doTasks = tasks.filter(item => item.isDone === false);
+
+    return (<div className={styles.wrap}>
+        <h1 className={styles.title}>Важные Дела:</h1>
+        <InputItem />
+        <ItemList task={tasks}/>
+        <Footer count ={doTasks.length}/>
+    </div>
+    )
+}
 export default App;
--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import styles from './InputItem.module.css';
 
-const InputItem = () => (<div>
-  <TextField
+const InputItem = () => (<div className ={styles.textField}> 
+  <TextField 
       id="standard-with-placeholder"
       label="Добавить новое задание"
       placeholder="Задание"
