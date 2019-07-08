93a477b3382ae5906a7c8b9af22c225730338302
1Amdro <halgadam@gmail.com>
2019-07-08 16:12:58 +0300

    component-class

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,35 +4,37 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 import InputItem from '../InputItem/InputItem';
 
-const App = () => {
-
-    const tasks = [
-        {
-            text: 'Повысить мотивацию',
-            isDone: true
-        },
-        {
-            text: 'Сделать задачу',
-            isDone: false
-        },
-        {
-            text: 'Отдохнуть',
-            isDone: true
-        },
-        {
-            text: 'Играть',
-            isDone: false
-        }
-    ]
-
-    let doTasks = tasks.filter(item => item.isDone === false);
-
-    return (<div className={styles.wrap}>
-        <h1 className={styles.title}>Важные Дела:</h1>
-        <InputItem />
-        <ItemList task={tasks}/>
-        <Footer count ={doTasks.length}/>
-    </div>
-    )
+class App extends React.Component {
+    render() {
+        const tasks = [
+            {
+                text: 'Повысить мотивацию',
+                isDone: true
+            },
+            {
+                text: 'Сделать задачу',
+                isDone: false
+            },
+            {
+                text: 'Отдохнуть',
+                isDone: true
+            },
+            {
+                text: 'Играть',
+                isDone: false
+            }
+        ];
+    
+        let doTasks = tasks.filter(item => item.isDone === false);
+    
+        return (<div className={styles.wrap}>
+            <h1 className={styles.title}>Важные Дела:</h1>
+            <InputItem />
+            <ItemList task={tasks}/>
+            <Footer count ={doTasks.length}/>
+        </div>
+        )
+    }
 }
+
 export default App;
