fd87a1ad75bf7699b4ca87a3ed4713200b5b8a2e
1Amdro <halgadam@gmail.com>
2019-07-22 17:49:41 +0300

    lyfecycle

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,33 +4,48 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Checkbox from '@material-ui/core/Checkbox';
 
-const Item = ({taskText, taskDone, taskId, onClickDone, onClickDelete}) => (
-    <div className={styles.listItem}>
-    <Checkbox
-        value="checkedA"
-        checked={taskDone}
-        onClick={() => onClickDone(taskId)}
-        inputProps={{
-          'aria-label': 'primary checkbox',
-        }}
-      />
-<div className=
-    {classnames({
-        [styles.text]: true,
-        [styles.done]: taskDone
-    })}
+class Item extends React.Component {
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+    }
+    componentWillUnmount(){
+        console.log('componentWillUnmount');
+    }
+    render() {
+        const {taskText, taskDone, taskId, onClickDone, onClickDelete} = this.props;
 
->{taskText}</div>
-<Checkbox
-        value="checkedF"
-        indeterminate
-        checked={taskDone}
-        onClick={() => onClickDelete(taskId)}
-        inputProps={{
-          'aria-label': 'indeterminate checkbox',
-        }}
-      />
-</div>)
+        return (
+            <div className={styles.listItem}>
+            <Checkbox
+                value="checkedA"
+                checked={taskDone}
+                onClick={() => onClickDone(taskId)}
+                inputProps={{
+                  'aria-label': 'primary checkbox',
+                }}
+              />
+        <div className=
+            {classnames({
+                [styles.text]: true,
+                [styles.done]: taskDone
+            })}
+        
+        >{taskText}</div>
+        <Checkbox
+                value="checkedF"
+                indeterminate
+                checked={taskDone}
+                onClick={() => onClickDelete(taskId)}
+                inputProps={{
+                  'aria-label': 'indeterminate checkbox',
+                }}
+              />
+        </div>)
+    }
+}
 
 Item.defaultProps = {
     taskDone: false
