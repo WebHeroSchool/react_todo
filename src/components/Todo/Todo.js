49b8deaa4359dfe55c0d4d2df259226eca44a2ea
Adam Khalguev <halgadam@gmail.com>
2020-07-25 19:31:55 +0300

    hooks

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,113 +1,112 @@
-import React from 'react';
+import React, {useEffect, useLayoutEffect, useState} from 'react';
 import styles from './Todo.module.css';
-import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import ItemListFilter from '../ItemListFilter/ItemListFilter';
 
 export const DataContext = React.createContext();
 
-class Todo extends React.Component {
-    state = {
-        tasks: [
-            {
-                text: 'СДЕЛАТЬ ЧТО-ТО',
-                isDone: true,
-                id: 1,
-                editor: false
+const Todo = () => {
 
-            },
-            {
-                text: 'REACT',
-                isDone: true,
-                id: 2,
-                editor: false
-            }
-        ],
-        count: 2
-    };
+    const [count, setCount] = useState(2)
+    const [tasks, setTasks] = useState([
+        {
+            text: 'СДЕЛАТЬ ЧТО-ТО',
+            isDone: true,
+            id: 1,
+            editor: false
 
-    componentWillMount() {
-        localStorage.getItem('tasks') && this.setState({
-            tasks: JSON.parse(localStorage.getItem('tasks')),
-            count: JSON.parse(localStorage.getItem('count'))
-        });
-    }
+        },
+        {
+            text: 'REACT',
+            isDone: true,
+            id: 2,
+            editor: false
+        }
+    ])
 
-        componentWillUpdate(nextProps, nextState, nextContext) {
-            localStorage.setItem('tasks', JSON.stringify(nextState.tasks));
-            localStorage.setItem('count', JSON.stringify(nextState.count));
+    useEffect(() => {
+        localStorage.setItem('my-tasks', JSON.stringify(tasks))
+        localStorage.setItem('my-counts', JSON.stringify(count))
+        console.log('update')
+    })
+
+    useLayoutEffect(() => {
+        console.log('mount')
+        const data = localStorage.getItem('my-tasks')
+        const dataCount = localStorage.getItem('my-counts')
+        if (data) {
+            setTasks(JSON.parse(data))
+            setCount(JSON.parse(dataCount))
         }
 
-    onClickDone = id => this.setState(state => {
-        const newItemList = state.tasks.map(item => {
+    }, [])
+
+    const onClickDone = id => {
+        const newItemList = tasks.map(item => {
             const newItem = {...item};
             if (item.id === id) {
                 newItem.isDone = !item.isDone;
             }
             return newItem;
         });
-        return {tasks: newItemList}
-    });
+        setTasks(newItemList)
+    }
 
-    enableEditor = id => this.setState(state => {
-        const newItemList = state.tasks.map(item => {
+    const enableEditor = id => {
+        const newItemList = tasks.map(item => {
             const newItem = {...item};
             if (item.id === id) {
                 newItem.editor = !item.editor;
             }
             return newItem
         });
-        return {tasks: newItemList}
-    });
+        setTasks(newItemList)
+    }
 
-    onClickDelete = id => this.setState(state =>
-        ({tasks: state.tasks.filter(item => item.id !== id)}));
+    const onClickDelete = id => {
+        setTasks(tasks.filter(item => item.id !== id))
+    }
 
-    onClickChange = (id, text )=> this.setState(state => {
-        const newItemList = state.tasks.map(item => {
+    const onClickChange = (id, text) => {
+        const newItemList = tasks.map(item => {
             const newItem = {...item};
             if (item.id === id) {
                 newItem.text = text
             }
             return newItem
         });
-        return {tasks: newItemList}
-    });
-
-
-    onClickAdd = text => this.setState(state => {
-        return {
-            tasks:[
-                ...state.tasks,
-                {
-                    text,
-                    isDone: false,
-                    id: state.count + 1,
-                    editor: false
-                }
-            ],
-            count: state.count + 1
-        }
-    });
-
-    render() {
-        const tasksText = this.state.tasks.map(item => item.text);
-        return (<div className={styles.main}>
-                <div className={styles.container}>
-                    <header className={styles.title}>Список важных задач</header>
-                    <InputItem onClickAdd={this.onClickAdd} tasksText={tasksText}/>
-                    <DataContext.Provider value={{
-                        tasksText: tasksText,
-                        onClickDone: this.onClickDone,
-                        onClickChange: this.onClickChange,
-                        onClickDelete: this.onClickDelete,
-                        enableEditor: this.enableEditor}}>
-                        <ItemListFilter tasks={this.state.tasks}/>
-                    </DataContext.Provider>
-                </div>
-            </div>
-        )
+        setTasks(newItemList)
     }
+
+    const onClickAdd = text => {
+        setTasks([
+            ...tasks,
+            {
+                text,
+                isDone: false,
+                id: count + 1,
+                editor: false
+            }
+        ])
+        setCount(count + 1)
+    }
+
+    const tasksText = tasks.map(item => item.text);
+    return (<div className={styles.main}>
+            <div className={styles.container}>
+                <header className={styles.title}>Список важных задач</header>
+                <InputItem onClickAdd={onClickAdd} tasksText={tasksText}/>
+                <DataContext.Provider value={{
+                    tasksText: tasksText,
+                    onClickDone: onClickDone,
+                    onClickChange: onClickChange,
+                    onClickDelete: onClickDelete,
+                    enableEditor: enableEditor}}>
+                    <ItemListFilter tasks={tasks}/>
+                </DataContext.Provider>
+            </div>
+        </div>
+    )
 }
 
 export default Todo;
